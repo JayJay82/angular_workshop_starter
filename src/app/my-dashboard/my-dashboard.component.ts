588c8d618949eee25f68d8e929d7f840a51c99f0
Costantino Cavallo <c.cavallo@uario.com>
2018-07-23 10:35:25 +0200

    Kick Off

---
+++ src/app/my-dashboard/my-dashboard.component.ts
@@ -0,0 +1,23 @@
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+
+@Component({
+  selector: 'my-dashboard',
+  templateUrl: './my-dashboard.component.html',
+  styleUrls: ['./my-dashboard.component.css']
+})
+export class MyDashboardComponent implements OnInit, OnDestroy {
+  
+
+  constructor() {}
+  
+  ngOnInit(): void {
+   
+    
+  }
+  ngOnDestroy(): void {
+     
+  }
+  
+}
